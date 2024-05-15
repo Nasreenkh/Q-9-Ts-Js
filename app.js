de63b7d3473004cfa7ee8e79442b19c1a7b64961
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 00:28:24 +0500

    This is 45 Question Assignment Q-9.
    This is 45 Question Assignment Q-9.

---
+++ app.js
@@ -0,0 +1,2 @@
+var favouriteNumber = 22;
+console.log("My favurite number is ".concat(favouriteNumber));
---
+++ app.ts
@@ -0,0 +1,8 @@
+//question no 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a
+//message that reveals your favorite number. Print that message.
+
+
+
+
+let favouriteNumber: number = 22;
+console.log(`My favurite number is ${favouriteNumber}`);
